9e59d4395371c21ca95108d499517610e8361113
Ans <ansibrahim@tutanota.com>
2025-02-04 15:27:27 +0500

    indented and fixed minor loopholes

--- no-time-for-caution@ans-ibrahim.github/extension.js
+++ no-time-for-caution@ans-ibrahim.github/extension.js
@@ -1,103 +1,120 @@
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import GLib from 'gi://GLib';
+import GObject from "gi://GObject";
+import St from "gi://St";
+import Clutter from "gi://Clutter";
+import GLib from "gi://GLib";
 
-import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
+  class Indicator extends PanelMenu.Button {
     _init(settings) {
-        super._init(0.0, _('No Time For Caution'));
-        this.settings = settings;
+      super._init(0.0, _("No Time For Caution"));
+      this.settings = settings;
 
-        this.label = new St.Label({ text: _('Calculating...'), y_align: Clutter.ActorAlign.CENTER });
-        this.add_child(this.label);
+      this.label = new St.Label({
+        text: _("Calculating..."),
+        y_align: Clutter.ActorAlign.CENTER,
+      });
+      this.add_child(this.label);
 
+      this._updateCountdown();
+      this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
         this._updateCountdown();
-        this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
-            this._updateCountdown();
-            return GLib.SOURCE_CONTINUE;
-        });
-
+        return GLib.SOURCE_CONTINUE;
+      });
     }
 
     _updateCountdown() {
-        let goalUnix = this.settings.get_int('goal-time');
-    
-        // Convert goal time to local time
-        let goalLocal = GLib.DateTime.new_from_unix_utc(goalUnix);
-    
-        let now = GLib.DateTime.new_now_utc();
-    
-        // Calculate time difference
-        let diff = goalLocal.to_unix() - now.to_unix();
-    
-        if (diff <= 0) {
-            this.label.set_text("No Goal");
-        } else {
-            let unit = this.settings.get_string('time-unit');
-            let timeString;
-    
-            switch (unit) {
-                case 'years':
-                    timeString = `${(diff / 31536000).toFixed(2)} Years Remaining`;
-                    break;
-                case 'months':
-                    timeString = `${(diff / 2592000).toFixed(2)} Months Remaining`;
-                    break;
-                case 'weeks':
-                    timeString = `${(diff / 604800).toFixed(2)} Weeks Remaining`;
-                    break;
-                case 'days':
-                    timeString = `${(diff / 86400).toFixed(2)} Days Remaining`;
-                    break;
-                case 'hours':
-                    timeString = `${(diff / 3600).toFixed(2)} Hours Remaining`;
-                    break;
-                case 'minutes':
-                    timeString = `${(diff / 60).toFixed(2)} Minutes Remaining`;
-                    break;
-                case 'seconds':
-                    timeString = `${diff} Seconds Remaining`;
-                default:
-                    timeString = `${diff} Seconds Remaining`;
-            }
-            this.label.set_text(timeString);
+      let goalUnix = this.settings.get_int("goal-time");
+
+      // Convert goal time to local time
+      let goalLocal = GLib.DateTime.new_from_unix_utc(goalUnix);
+
+      let now = GLib.DateTime.new_now_utc();
+
+      // Calculate time difference
+      let diff = goalLocal.to_unix() - now.to_unix();
+
+      if (diff <= 0) {
+        this.label.set_text("No Goal");
+      } else {
+        let unit = this.settings.get_string("time-unit");
+        let timeString;
+
+        switch (unit) {
+          case "years":
+            timeString = `${(diff / 31536000).toFixed(2)} Years Remaining`;
+            break;
+          case "months":
+            timeString = `${(diff / 2592000).toFixed(2)} Months Remaining`;
+            break;
+          case "weeks":
+            timeString = `${(diff / 604800).toFixed(2)} Weeks Remaining`;
+            break;
+          case "days":
+            timeString = `${(diff / 86400).toFixed(2)} Days Remaining`;
+            break;
+          case "hours":
+            timeString = `${(diff / 3600).toFixed(2)} Hours Remaining`;
+            break;
+          case "minutes":
+            timeString = `${(diff / 60).toFixed(2)} Minutes Remaining`;
+            break;
+          case "seconds":
+            timeString = `${diff} Seconds Remaining`;
+          default:
+            timeString = `${diff} Seconds Remaining`;
         }
+        this.label.set_text(timeString);
+      }
     }
 
     destroy() {
-        if (this._timeout) {
-            GLib.source_remove(this._timeout);
-            this._timeout = null;
-        }
-        super.destroy();
+      if (this._timeout) {
+        GLib.source_remove(this._timeout);
+        this._timeout = null;
+      }
+      super.destroy();
     }
-});
+  }
+);
 
 export default class NoTimeForCautionExtension extends Extension {
-    enable() {
-        this.settings = this.getSettings();
-        this._indicator = new Indicator(this.settings);
-        Main.panel.addToStatusArea('no-time-for-caution@ans-ibrahim.github',this._indicator,this.settings.get_int('indicator-index'),this.settings.get_string('indicator-position'));
-        this.settings.connect('changed', () => {
-            if (this._indicator) {
-                this._indicator.destroy();
-                this._indicator = null;
-            }
-            this._indicator = new Indicator(this.settings);
-            Main.panel.addToStatusArea('no-time-for-caution@ans-ibrahim.github',this._indicator,this.settings.get_int('indicator-index'),this.settings.get_string('indicator-position'));
-        });
-    }
+  enable() {
+    this.settings = this.getSettings();
+    this._indicator = new Indicator(this.settings);
+    Main.panel.addToStatusArea(
+      "no-time-for-caution@ans-ibrahim.github",
+      this._indicator,
+      this.settings.get_int("indicator-index"),
+      this.settings.get_string("indicator-position")
+    );
+    this.settings.connect("changed", () => {
+      if (this._indicator) {
+        this._indicator.destroy();
+        this._indicator = null;
+      }
+      this._indicator = new Indicator(this.settings);
+      Main.panel.addToStatusArea(
+        "no-time-for-caution@ans-ibrahim.github",
+        this._indicator,
+        this.settings.get_int("indicator-index"),
+        this.settings.get_string("indicator-position")
+      );
+    });
+  }
 
-    disable() {
-        if (this._indicator) {
-            this.settings.disconnect('changed');
-            this._indicator.destroy();
-            this._indicator = null;
-        }
+  disable() {
+    if (this._indicator) {
+      this.settings.disconnect("changed");
+      this.settings = null;
+      this._indicator.destroy();
+      this._indicator = null;
     }
+  }
 }
--- no-time-for-caution@ans-ibrahim.github/prefs.js
+++ no-time-for-caution@ans-ibrahim.github/prefs.js
@@ -3,9 +3,13 @@ import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import Gtk from "gi://Gtk";
 
-import { ExtensionPreferences, gettext as _ } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+import {
+  ExtensionPreferences,
+  gettext as _,
+} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class NoTimeForCautionPreferences extends ExtensionPreferences {
+
   fillPreferencesWindow(window) {
     const settings = this.getSettings();
 
@@ -21,67 +25,99 @@ export default class NoTimeForCautionPreferences extends ExtensionPreferences {
     const goalTimeRow = new Adw.ActionRow({
       title: _("Goal Time"),
       subtitle: _("Enter in format: DD/MM/YYYY hh:mm"),
-  });
-  generalGroup.add(goalTimeRow);
-  
-  const goalTimeEntryBuffer = new Gtk.EntryBuffer();
-  const goalTimeEntry = new Gtk.Entry({
+    });
+    generalGroup.add(goalTimeRow);
+
+    const goalTimeEntryBuffer = new Gtk.EntryBuffer();
+    const goalTimeEntry = new Gtk.Entry({
       buffer: goalTimeEntryBuffer,
       placeholder_text: _("DD/MM/YYYY hh:mm"),
-  });
-  
-  goalTimeRow.add_suffix(goalTimeEntry);
-  goalTimeRow.set_activatable_widget(goalTimeEntry);
-  
-  // Load stored goal time and format it
-  const storedTime = settings.get_int("goal-time");
-  if (storedTime > 0) {
+    });
+
+    goalTimeRow.add_suffix(goalTimeEntry);
+    goalTimeRow.set_activatable_widget(goalTimeEntry);
+
+    // Load stored goal time and format it
+    const storedTime = settings.get_int("goal-time");
+    if (storedTime > 0) {
       const storedDate = GLib.DateTime.new_from_unix_local(storedTime);
       goalTimeEntryBuffer.set_text(storedDate.format("%d/%m/%Y %H:%M"), -1);
-  }
-  
-  // Validate and save input
-  goalTimeEntry.connect("changed", () => {
+    }
+
+    // Validate and save input
+    goalTimeEntry.connect("changed", () => {
       const input = goalTimeEntryBuffer.get_text().trim();
       const regex = /^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})$/;
       const match = input.match(regex);
-  
+
       if (match) {
-          const [, day, month, year, hour, minute] = match.map(Number);
-          const dateTime = GLib.DateTime.new_local(year, month, day, hour, minute, 0).to_utc();
-          const unixTimestamp = dateTime.to_unix();
-          settings.set_int("goal-time", unixTimestamp);
+        const [, day, month, year, hour, minute] = match.map(Number);
+        const dateTime = GLib.DateTime.new_local(
+          year,
+          month,
+          day,
+          hour,
+          minute,
+          0
+        ).to_utc();
+        const unixTimestamp = dateTime.to_unix();
+        settings.set_int("goal-time", unixTimestamp);
       }
-  });
-  
+    });
+
     // Time Unit
     const timeUnitOptions = new Gtk.StringList();
-    ["years", "months", "weeks", "days", "hours", "minutes", "seconds"].forEach((unit) => timeUnitOptions.append(_(unit)));
+    ["years", "months", "weeks", "days", "hours", "minutes", "seconds"].forEach(
+      (unit) => timeUnitOptions.append(_(unit))
+    );
 
     const timeUnitRow = new Adw.ComboRow({
       title: _("Time Unit"),
       subtitle: _("Unit of time to display"),
       model: timeUnitOptions,
-      selected: ["years", "months", "weeks", "days", "hours", "minutes", "seconds"].indexOf(settings.get_string("time-unit")),
+      selected: [
+        "years",
+        "months",
+        "weeks",
+        "days",
+        "hours",
+        "minutes",
+        "seconds",
+      ].indexOf(settings.get_string("time-unit")),
     });
+
     timeUnitRow.connect("notify::selected", (row) => {
-      settings.set_string("time-unit", ["years", "months", "weeks", "days", "hours", "minutes", "seconds"][row.selected]);
+      settings.set_string(
+        "time-unit",
+        ["years", "months", "weeks", "days", "hours", "minutes", "seconds"][
+          row.selected
+        ]
+      );
     });
     generalGroup.add(timeUnitRow);
 
     // Indicator Position
     const positionOptions = new Gtk.StringList();
-    ["left", "center", "right"].forEach((pos) => positionOptions.append(_(pos)));
+    ["left", "center", "right"].forEach((pos) =>
+      positionOptions.append(_(pos))
+    );
 
     const positionRow = new Adw.ComboRow({
       title: _("Indicator Position"),
       subtitle: _("Position of the indicator in the panel"),
       model: positionOptions,
-      selected: ["left", "center", "right"].indexOf(settings.get_string("indicator-position")),
+      selected: ["left", "center", "right"].indexOf(
+        settings.get_string("indicator-position")
+      ),
     });
+
     positionRow.connect("notify::selected", (row) => {
-      settings.set_string("indicator-position", ["left", "center", "right"][row.selected]);
+      settings.set_string(
+        "indicator-position",
+        ["left", "center", "right"][row.selected]
+      );
     });
+
     generalGroup.add(positionRow);
 
     // Indicator Index
@@ -89,17 +125,29 @@ export default class NoTimeForCautionPreferences extends ExtensionPreferences {
       title: _("Indicator Index"),
       subtitle: _("Index of the indicator in the panel"),
     });
+
     generalGroup.add(indexRow);
 
     const indexSpinButton = new Gtk.SpinButton({
-      adjustment: new Gtk.Adjustment({ lower: 0, upper: 10, step_increment: 1 }),
+      adjustment: new Gtk.Adjustment({
+        lower: 0,
+        upper: 10,
+        step_increment: 1,
+      }),
       numeric: true,
       marginTop: 10,
       marginBottom: 10,
     });
+
+    
     indexRow.add_suffix(indexSpinButton);
     indexRow.set_activatable_widget(indexSpinButton);
 
-    settings.bind("indicator-index", indexSpinButton, "value", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "indicator-index",
+      indexSpinButton,
+      "value",
+      Gio.SettingsBindFlags.DEFAULT
+    );
   }
 }
